be010ff25d4cfa4e21a491ead8cbf2d9edef7379
atomicwest <ogjdne@gmail.com>
2015-11-16 12:02:09 -0800

    Update scripthov.js

--- resources/js/scripthov.js
+++ resources/js/scripthov.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $('div').mouseenter(function() {
-        $('div').fadeTo('fast', 1)
+    $('ul').mouseenter(function() {
+        $('ul').fadeTo('fast', 1)
     });
-    $('div').mouseleave(function() {
-        $('div').fadeTo('fast', 0.5)
+    $('ul').mouseleave(function() {
+        $('ul').fadeTo('fast', 0.5)
     });
 });
