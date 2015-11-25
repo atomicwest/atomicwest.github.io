cca43e510ea2c7732cbfaab534e5b31e6e807aaa
atomicwest <ogjdne@gmail.com>
2015-11-25 00:31:28 -0800

    Update memcheck4.py

--- extra/memcheck4.py
+++ extra/memcheck4.py
@@ -8,49 +8,47 @@ import os
 import platform
 import sys
 
-#regular expressions e.g. search
-import re
-
-#import psutil
-
-#return namedtuple of memory in bytes
-#mem = psutil.virtual_memory()
-
-#1. Total system memory 
-
-#output variable
-#tsm = mem.total
-#tsm = "400"
-#2. Total system memory in use
-
-#output variable
-#tsmIU = mem.used
-#tsmIU = "500"
-#3. Total system memory available
-
-#for windows, available and free are the same parameter
-
-#output variable
-#tsmAV = mem.available
-#tsmAV = 800
+#for using MEMORYSTATUSEX
+import ctypes
 
 #------------------------------------------------------------------
 
 #if windows is the detected operating system
 #def memCheck():
-if 'Windows' in platform.system() :
+if 'Windows' in platform.system():
     #run the win32 library
     #in use memory = total - available?
-    tsm = "output from the Windows commands"
-    tsmIU = "output from the Windows commands"
-    tsmAV = "output from the Windows commands"
+    class MEMORYSTATUSEX(ctypes.Structure):
+        _fields_ = [
+            ("dwLength", ctypes.c_ulong),
+            ("dwMemoryLoad", ctypes.c_ulong),
+            ("ullTotalPhys", ctypes.c_ulonglong),
+            ("ullAvailPhys", ctypes.c_ulonglong),
+            ("ullTotalPageFile", ctypes.c_ulonglong),
+            ("ullAvailPageFile", ctypes.c_ulonglong),
+            ("ullTotalVirtual", ctypes.c_ulonglong),
+            ("ullAvailVirtual", ctypes.c_ulonglong),
+            ("sullAvailExtendedVirtual", ctypes.c_ulonglong),
+            ]
+
+        def __init__(self):
+            #initialize to size of MEMORYSTATUSEX
+            self.dwLength = ctypes.sizeof(self)
+            super(MEMORYSTATUSEX, self).__init__()
+
+    stat = MEMORYSTATUSEX()
+    ctypes.windll.kernel32.GlobalMemoryStatusEx(ctypes.byref(stat))
+
+    tsm = stat.ullTotalPhys / 1024
+    tsmAV = stat.ullAvailPhys / 1024
+    tsmIU = tsm - tsmAV
     
 elif 'Linux' in platform.system():
     #read out of /proc/meminfo
     meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
     #total system memory
     tsm = meminfo['MemTotal']
-    #total available memory
+    #total available memory, assuming MemFree also contains Cached and/or buffer
     tsmAV = meminfo['MemFree']
     #total memory in use, assuming that it is a simple difference
     #between total and available
@@ -58,15 +56,17 @@ elif 'Linux' in platform.system():
 else:
     print("Unrecognized OS")
     
-#------------------------------------------------------------------
-
-#in Linux, read from /proc/meminfo
+#test by printing variables to the console
+print("Total system memory: %d kB" % tsm)
+print("Total available system memory: %d kB" % tsmAV)
+print("Total system memory in use: %d kB" % tsmIU) 
 
 #------------------------------------------------------------------
 
 #output 1-3 in .txt file
 outhand = open("Memout.txt", "w")
-outhand.write("Total System Memory: %s kB \n" % tsm)
-outhand.write("Total System Memory in use: %s kB \n" % tsmIU)
-outhand.write("Total System Memory available: %s kB \n" % tsmAV)
+#outhand.write("Total System Memory: %s \nTotal System Memory in Use: %s \nTotal System Memory Available: %s" % (tsm, tsmIU, tsmAV))
+outhand.write("Total System Memory: %s bytes \n" % tsm)
+outhand.write("Total System Memory in use: %s bytes \n" % tsmIU)
+outhand.write("Total System Memory available: %s bytes \n" % tsmAV)
 outhand.close()
