9f24eba1f6322bf410ab81c746bcc1001f6f2a19
Jesson Go <ogjdne@gmail.com>
2016-12-13 05:02:12 +0000

    fix toOther() method and tested fromDecimal() and toDecimal()

--- resources/number_converter/js/conversion_use.js
+++ resources/number_converter/js/conversion_use.js
@@ -6,14 +6,11 @@
 // 1. decimal to other base system
 // inval is starting number, sys is the output number system
 function fromDecimal(inval, sys){
-
-  // if (! checkBase(inval,sys)) {return 0}
-
   //force default system
   if (sys<=1 || sys==undefined){ sys = 2 }
 
   var output = ""
-  var nextval = inval
+  var nextval = parseInt(inval)
   var digit
 
   while (nextval!=0){
@@ -24,29 +21,23 @@ function fromDecimal(inval, sys){
     output = digit.toString() + output
     nextval = Math.floor(nextval/sys)
   }
-
+  if (inval=="0") {output = "0"}
   return output
 
 }
-
 
 //------------------------------------------------------
 // 2. other base system to decimal
 // orig is the base system of the input inval
 function toDecimal(inval, orig){
-  // if (! checkBase(inval,orig)) {return 0}
-  //force default system
-  if (orig<=1 || orig==undefined){ orig = 2 }
 
-  var digits = inval.toString().split("").reverse().join("")
-  var total = 0
+  var numalpha = numalph();
+  var inputvalue = inval.toString();
+  var digits = inputvalue.toUpperCase().split("").reverse().join("");
+  var total = 0;
 
   for(var i=0; i<digits.length; i++){
-    if (digits[i] < 10){
-        total += parseInt(digits[i]) * Math.pow(orig,i)
-    } else if (typeof(digits[i])=="string"){
-        total += digits[i].charCodeAt() * Math.pow(orig,i)
-    }
+    total += numalpha[digits[i]] * Math.pow(orig,i)
   }
 
   return total.toString()
@@ -58,31 +49,36 @@ function toDecimal(inval, orig){
 // orig is the starting base, outsys is the output base
 function toOther(){
 
-  var inval = parseInt(document.getElementById("convfrom").value)
-  var orig = parseInt(document.getElementById("origbase").value)
-  var outsys = parseInt(document.getElementById("endbase").value)
-  var output
+  var inval = document.getElementById("convfrom").value.toUpperCase();
+  var orig = document.getElementById("origbase").value.toUpperCase();
+  var outsys = document.getElementById("endbase").value.toUpperCase();
 
-  //force default systems
-  if (orig<=1 || orig==undefined){ orig = 2 }
-  if (outsys<=1 || outsys==undefined){ outsys = 2 }
-
-  var convert
-  if (orig==10){
-    output = fromDecimal(inval, outsys)
-  } else if (orig<=0) {
+  if (! isNum(orig)) {
+    alert('Original radix contains letters or is blank')
+    document.getElementById("origbase").focus();
     return
-  } else {
-    convert = parseInt(toDecimal(inval,orig))
-    output = fromDecimal(convert,outsys)
+  } else if (! isNum(outsys)) {
+    alert('Final radix contains letters or is blank')
+    document.getElementById("endbase").focus();
+    return
   }
 
-  if (inval==0){
-      document.getElementById("result").value = "0"
+  var output;
+
+  var convert;
+  if (orig==10){
+    output = fromDecimal(inval, outsys);
+  } else {
+    convert = toDecimal(inval,orig);
+    output = fromDecimal(convert,outsys);
+  }
+
+  if (inval=="0"){
+      document.getElementById("result").value = "0";
   } else if (! checkBase(inval, orig)) {
-      document.getElementById("result").value = "Re-enter input number and/or base"
+      document.getElementById("result").value = "Re-enter input and/or base";
   } else if (! checkBase(output, outsys)) {
-      document.getElementById("result").value = "Re-enter output base"
+      document.getElementById("result").value = "Re-enter output base";
   } else {
       document.getElementById("result").value = output
   }
@@ -106,14 +102,14 @@ function hexprefix(){
 }
 
 //------------------------------------------
-// raises alert and returns False if digits are larger than the base value
+// raises alert and returns False if input digits are larger than the base value
 function checkBase(number, base){
-  var strArr = number.toString().split("")
+  var numalpha = numalph()
+  var thisnum = number.toString()
+  var strArr = thisnum.toUpperCase().split("")
   for (var i=0; i<strArr.length; i++) {
-    if (parseInt(strArr[i]) >= base) {
+    if ((numalpha[strArr[i]] >= base) || numalpha[strArr[i]]==null) {
       alert("One or more digits are larger than the base value.\nPlease enter a valid input/base combination")
-      console.log(number)
-      console.log(base)
       return false
     }
   }
@@ -126,4 +122,29 @@ function clearfield(){
   document.getElementById("origbase").value = 2
   document.getElementById("endbase").value = 2
   document.getElementById("result").value = 0
+}
+
+//------------------------------------------
+function isNum(value) {
+  var nums = ["0","1","2","3","4","5","6","7","8","9"]
+  var splitval = value.split("")
+  var check = true
+  if (value.length==0) { check = false }
+  splitval.forEach(function(item) {
+      // debugger
+      if ($.inArray(item,nums)===-1) { check = false }
+  })
+  return check
+}
+
+//------------------------------------------
+function numalph() {
+  var obj={};
+  for (var j=0; j<10; j++){
+    obj[j.toString()] = j
+  }
+  for (var v=10; v<16; v++) {
+    obj[String.fromCharCode(v+55)] = v
+  }
+  return obj
 }
