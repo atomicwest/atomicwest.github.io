8b814872e2f4478b6c812b1b3a22d33a0a4993e7
Jesson Go <ogjdne@gmail.com>
2017-11-28 06:56:06 +0000

    update shell copy

--- extra/python-utils/tstcopy.ps1
+++ extra/python-utils/tstcopy.ps1
@@ -1,10 +1,24 @@
-<# move multiple files out of multiple folders 
-and rename with .png extension #>
- 
-$dir = "C:\"
+<# 
+JG
+powershell -ExecutionPolicy bypass
+- move multiple files out of multiple folders 
+- rename with .png extension (assuming files have no ext)
+- change search dir and new dir as necessary
+#>
 
-Get-ChildItem -Recurse $dir | ForEach-Object {
-	#Write-Host $_.Name
-	Copy-Item $_.Name -Destination "$($dir)\newroot"
+$searchdir = "rootdir"
+$newdir = "copyroot"
+
+$searchpath = "$($pwd)\$($searchdir)"
+$newpath = "$($pwd)\$($newdir)"
+
+If (-Not(Test-Path $newpath)) {
+	New-Item -Path $pwd -Name $newdir -ItemType "directory"
 }
 
+Get-ChildItem -Recurse $searchpath | ForEach-Object {
+	If (-Not($_ -is [System.IO.DirectoryInfo] )) {
+		#Write-Host $_.Name
+		Copy-Item $_.FullName -Destination "$($newpath)\$($_.Name).png"
+	}
+}
