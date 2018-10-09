9d2745e81705b50b141f89a3704162a7fd9686e7
Jesson Go <ogjdne@gmail.com>
2018-10-09 06:22:47 +0000

    update SQL with proper example

--- extra/sql/example-SUM-sort.sql
+++ extra/sql/example-SUM-sort.sql
@@ -23,8 +23,23 @@ SELECT * FROM (VALUES
 AS sampleTable(orderID, name, spending, spendDate)
 
 
---attempt SUM  as a filter condition
+--attempt SUM  as a filter condition; the WRONG way
 SELECT name, SUM(spending)
 FROM #tmpOrders
 WHERE SUM(spending) > 100
+--throws this error: An aggregate may not appear in the WHERE clause unless it is in a subquery contained in a HAVING clause or a select list, and the column being aggregated is an outer reference.
 
+
+--using SUM with GROUP BY and HAVING; the proper way
+SELECT name, SUM(spending)
+FROM #tmpOrders
+Group By spending, name HAVING  SUM(spending) > 100
+
+/*
+name	(No column name)
+Bob	    1488
+Jack	136
+Jane	450
+Jane	1580
+Jane	1770
+*/
